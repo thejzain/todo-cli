65eb40d86d656882e576909318f6379753e4915b
thejzain <thejzainkp@gmail.com>
2023-01-29 19:01:35 +0530

    add workin_

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,38 @@
-// use std::io;
+use std::collections::HashMap;
+use std::env;
 
-// struct Todo {
-//     active: bool,
-//     todo: String,
-// }
 
-// fn main() {
-//     let mut todos: Vec<Todo> = Vec::new();
-//     loop {
-//         let mut todos = &mut todos;
-//         let mut choice = String::new();
-//         println!("Choose\n 1.Add\n 2.Delete\n 3.Exit");
-//         io::stdin().read_line(&mut choice).expect("error");
-//         let num: i32 = choice.trim().parse().expect("Not an i32");
-//         if num == 4 {
-//             println!("Exit");
-//             break;
-//         } else if num == 1 {
-//             let mut todos = add_todo(&mut todos);
-//             for x in todos {
-//                 println!("{}", x.todo);
-//             }
-//         } else if num == 3 {
+struct Todo {
+    map: HashMap<String, bool>,
+}
 
-//         }else {
-//             println!("Invalid choice!!");
-//         }
-//     }
-// }
+impl Todo {
+    fn insert (&mut self, key: String){
+        self.map.insert(key, true);
+    }
 
-// fn add_todo(_todos: &mut Vec<Todo>) -> &mut Vec<Todo> {
-//     let mut note = String::new();
-//     println!("Enter your note : ");
-//     io::stdin().read_line(&mut note).expect("err");
-//     let todo = Todo {
-//         active: true,
-//         todo: String::from(note),
-//     };
-//     _todos.push(todo);
-//     return _todos;
-// }
-
+    fn save (self) -> Result<(),std::io::Error>{
+        let mut content = String::new();
+        for (k,v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
+}
 
 fn main(){
-    
+    let action: String = env::args().nth(1).expect("action err");
+    let item : String = env::args().nth(2).expect("item err");
+    let mut todo = Todo{
+        map: HashMap::new(),
+    };
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occured: {}", why),
+        }
+    }
+
 }
